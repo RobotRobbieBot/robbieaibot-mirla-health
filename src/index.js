8af6465f90d850bef7918ff7ca25f8b2e0c702d3
Robbie <robbie@example.com>
2026-05-22 09:34:15 -0400

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import MirlaHealthHub from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MirlaHealthHub />
+  </React.StrictMode>
+);
